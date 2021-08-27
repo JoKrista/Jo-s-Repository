9c40c5b93540f84de0e393a9af5d9aa0635b7d79
JoKrista <88526383+JoKrista@users.noreply.github.com>
2021-08-26 23:02:18 -0400

    Add files via upload

---
+++ DinerDashRestaurant/ContactUs.js
@@ -0,0 +1,10 @@
+function resetForm() {
+    clearErrors();
+}
+function validateItems() {
+    clearErrors();
+    var Name = document.forms["ContactUsForm"]["Name"].value;
+    var Email = document.forms["ContactUsForm"]["Email"].value;
+    var Phone = document.forms["ContactUsForm"]["Phone"].value;
+	
+   }
